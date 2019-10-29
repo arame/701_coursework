43d71dce1077dbdbc3361a342ca23c9aeec7f1c1
PG-Payne <Geoffrey.Payne@city.ac.uk>
2019-10-29 17:34:11 +0000

    intial version

---
+++ src/border_crossing.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib                  # 2D Plotting Library
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as mgrid
+import seaborn as sns  
+
+from files import Files
+from local_time import LocalTime
+
+t = LocalTime()
+print("---------------------------------------------------------")
+print("Local current time started :", t.localtime)
+print("---------------------------------------------------------")
+inputFile = "Border_Crossing_Entry_Data.csv"
+f = Files(inputFile)
+data = pd.read_csv(f.file_path)
+print(data.info())
+data.head()
+t = LocalTime()
+print("---------------------------------------------------------")
+print("Local current time completed :", t.localtime)
+print("---------------------------------------------------------")
+data.isnull().any()
---
+++ src/files.py
@@ -0,0 +1,5 @@
+class Files:
+    def __init__(self, file):
+        path = "U:\\701\\files\\"
+        self.file = file
+        self.file_path = path + file
