ded02997d0eaa4cd956ebedb6859266a8f666366
PG-Payne <Geoffrey.Payne@city.ac.uk>
2019-10-29 21:39:57 +0000

    Update pathname for file

--- src/files.py
+++ src/files.py
@@ -1,5 +1,6 @@
 class Files:
     def __init__(self, file):
-        path = "U:\\701\\files\\"
+        # U:\701\701_coursework\files
+        path = "U:\\701\\701_coursework\\files\\"
         self.file = file
         self.file_path = path + file
