9fdf1496eff577d63def1767c53160e9d613f783
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2019-12-02 15:23:00 +0000

    Refactor time caculation

--- src/Main.py
+++ src/Main.py
@@ -16,38 +16,36 @@ from datalook import Datalook
 from files import Files
 from local_time import LocalTime
 from textblob import TextBlob
-
-
 from wordcloud import WordCloud, STOPWORDS
 
 print('\n' * 10)
-t = LocalTime()
 print("=========================================================")
-print("Local current time started :", t.localtime)
+print("Local current time started :", LocalTime.get())
 print("=========================================================")
 twitter_file = "auspol2019.csv"
 f1 = Files(twitter_file)
-print(LocalTime().localtime, twitter_file, " read")
+print(LocalTime.get(), twitter_file, " read")
 twitter = pd.read_csv(f1.file_path, parse_dates=['created_at','user_created_at'])
 geocode_file = "location_geocode.csv"
 f2 = Files(geocode_file)
-print(LocalTime().localtime, geocode_file, " read")
+print(LocalTime.get(), geocode_file, " read")
 geocodes = pd.read_csv(f2.file_path)
 twitter = twitter.merge(geocodes, how='inner', left_on='user_location', right_on='name')
 twitter = twitter.drop('name',axis =1)  # 'name' is a duplicate of 'user location' so remove.
+print(LocalTime.get(), "files merged")
 # create training and testing vars
 train, test = train_test_split(twitter, test_size=0.2)
 print("-----------------------")
 print("Train data = ", train.shape)
 print("Test data = ", test.shape)
 print("-----------------------")
-print(LocalTime().localtime, "files merged")
+
 #data = Datalook(twitter)
 #data.show()
 
 #Wordcloudz.show(twitter, 'user_description')
 twitter['sentiment'] = twitter['full_text'].map(lambda text: TextBlob(text).sentiment.polarity)
-print(LocalTime().localtime, "5 random tweets with highest positive sentiment polarity: \n")
+print(LocalTime.get(), "5 random tweets with highest positive sentiment polarity: \n")
 cL = twitter.loc[twitter.sentiment==1, ['full_text']].sample(5).values
 positive_sentences = []
 for c in cL:
@@ -64,7 +62,7 @@ for c in cL:
 number_positive = len(positive_sentences)
 percentage_positive = "which is {0:.2f}% of all tweets".format((number_positive / len(twitter)) * 100)
 print("-----------------------")
-print(LocalTime().localtime, "Number of positive sentiments = ", number_positive, percentage_positive)
+print(LocalTime.get(), "Number of positive sentiments = ", number_positive, percentage_positive)
 
 negative_sentences = []
 cL = twitter.loc[twitter.sentiment==0, ['full_text']].values
@@ -77,7 +75,7 @@ for c in cL:
 number_negative = len(negative_sentences)
 percentage_negative = "which is {0:.2f}% of all tweets".format((number_negative / len(twitter)) * 100)
 print("-----------------------")
-print(LocalTime().localtime, "Number of negative sentiments = ", number_negative, percentage_negative)
+print(LocalTime.get(), "Number of negative sentiments = ", number_negative, percentage_negative)
 print("-----------------------")
 count_of_tweets = len(twitter)
 count_of_retweets = np.sum(twitter.retweet_count)
@@ -94,7 +92,6 @@ print("-----------------------")
 print(f"Most retweeted: ", twitter.loc[twitter['retweet_count']==6622.0,'full_text'].values)
 print("-----------------------")
 print(f"Most favourited: ",twitter.loc[twitter['favorite_count']==15559.0,['full_text','user_name','user_description']].values)
-t = LocalTime()
 print("=========================================================")
-print("Local current time completed :", t.localtime)
+print("Local current time completed :", LocalTime.get())
 print("=========================================================")
